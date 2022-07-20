2b72dcb88ee7659b85361492bba40bc010815263
elseif <fdfjonfmd@gmail.com>
2022-07-20 10:55:22 -0300

    att global

---
+++ assets/js/global.js
@@ -0,0 +1,58 @@
+// active function
+
+document.querySelectorAll('.navbar-item a').forEach(isActive => {
+    isActive.addEventListener('click', function(){
+        document.querySelector('.navbar-menu').querySelector('.active').classList.remove('active')
+        isActive.classList.add('active')
+    })
+});
+
+// click navbar
+
+document.querySelector('#mobile-menu').addEventListener('click',
+    () => {
+        document.querySelector('#mobile-menu').classList.toggle('is-active')
+        document.querySelector('.navbar-menu').classList.toggle('active')
+    }
+);
+
+// sliders
+
+const controls = document.querySelectorAll('.control');
+
+const items = document.querySelectorAll('.item');
+const bars = document.querySelectorAll('.sliders-bar');
+const maxItems = items.length;
+
+let curruentItem = 0;
+
+controls.forEach(control => {
+    control.addEventListener('click', () => {
+        const isLeft = control.classList.contains('arrow-left');
+
+        if(isLeft){
+            curruentItem -= 1;
+        }else{
+            curruentItem += 1;
+        };
+
+        if(curruentItem >= maxItems){
+            curruentItem = 0;
+        };
+
+        if(curruentItem < 0){
+            curruentItem = maxItems - 1;
+        };
+
+        items.forEach(item => item.classList.remove('curruent-item'));
+        bars.forEach(item => item.classList.remove('active'));
+
+        items[curruentItem].scrollIntoView({
+            inline: 'center',
+            behavior: 'smooth'
+        });
+
+        items[curruentItem].classList.add('curruent-item');
+        bars[curruentItem].classList.add('active');
+    });
+});
